21e1b457d51ad37ffaa83cd1f34d9979f14732be
Santiago <perejilinformatico@gmail.com>
2026-05-11 20:39:54 -0300

    Rediseño final

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from "@astrojs/node";
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    server: {
+      host: '0.0.0.0',
+      port: 3000
+    }
+  },
+  adapter: node({
+    mode: 'standalone'
+  })
 });
